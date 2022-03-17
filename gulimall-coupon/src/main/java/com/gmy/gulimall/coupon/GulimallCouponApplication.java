9d6f7a5cf91c459a98e85fcf2b988c9bf8e4f5d1
gmy1234 <1508594767@qq.com>
2022-03-17 14:23:46 +0800

    coupon 使用spring cloud alibaba Nacos 注册中心

--- gulimall-coupon/src/main/java/com/gmy/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/gmy/gulimall/coupon/GulimallCouponApplication.java
@@ -3,10 +3,13 @@ package com.gmy.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author gmyDL
  */
+
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.gmy.gulimall.coupon.dao")
 public class GulimallCouponApplication {
@@ -14,5 +17,4 @@ public class GulimallCouponApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallCouponApplication.class, args);
     }
-
 }
