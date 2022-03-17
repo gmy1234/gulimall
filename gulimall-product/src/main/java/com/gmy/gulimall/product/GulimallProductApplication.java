1657e6663589b8aece5616850d6118a5f770772e
gmy1234 <1508594767@qq.com>
2022-03-17 13:41:14 +0800

    设置商品服务的端口号，启动类上添加注解

--- gulimall-product/src/main/java/com/gmy/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/gmy/gulimall/product/GulimallProductApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.gmy.guliorder.product.dao")
+@MapperScan("com.gmy.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
