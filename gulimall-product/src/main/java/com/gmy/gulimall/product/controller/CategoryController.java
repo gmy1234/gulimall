2fae1f441efee32b22dd89a69140c96de1f5e4f1
gmy1234 <1508594767@qq.com>
2022-03-21 11:50:08 +0800

    API网关配置和跨域配置

--- gulimall-product/src/main/java/com/gmy/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/gmy/gulimall/product/controller/CategoryController.java
@@ -5,11 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.gmy.gulimall.product.entity.CategoryEntity;
 import com.gmy.gulimall.product.service.CategoryService;
