265d956fb4aa3ac0d82905c16ace08f09564bbd1
gmy1234 <1508594767@qq.com>
2022-03-22 14:46:53 +0800

    添加第三方服务包，管理OSS服务

--- gulimall-product/src/test/java/com/gmy/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/gmy/gulimall/product/GulimallProductApplicationTests.java
@@ -13,59 +13,9 @@ import java.io.InputStream;
 @SpringBootTest
 class GulimallProductApplicationTests {
 
-    @Autowired
-    OSS oss;
-
-    @Test
-    public void ossTest() throws FileNotFoundException {
-        String filePath= "D:\\下载内容\\one2.jpg";
-        InputStream inputStream = new FileInputStream(filePath);
-        oss.putObject("gulimall-gmy", "data/one2.jpg", inputStream);
-
-        oss.shutdown();
-        System.out.println("上传完成");
-    }
-
     @Test
     void contextLoads() {
-        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-        String endpoint = "oss-cn-beijing.aliyuncs.com";
-        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tLqPcndgvCmQ6hnqch9";
-        String accessKeySecret = "s1K8xXFvcZnAbd37RGlRBRHHSP0ztE";
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = "gulimall-gmy";
-        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        String objectName = "data/one.jpg";
-        // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
-        // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-        String filePath= "D:\\下载内容\\one.jpg";
 
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-        try {
-            InputStream inputStream = new FileInputStream(filePath);
-            // 创建PutObject请求。
-            ossClient.putObject(bucketName, objectName, inputStream);
-        } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-                    + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
-        } catch (ClientException ce) {
-            System.out.println("Caught an ClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with OSS, "
-                    + "such as not being able to access the network.");
-            System.out.println("Error Message:" + ce.getMessage());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (ossClient != null) {
-                ossClient.shutdown();
-            }
-        }
     }
 
 }
