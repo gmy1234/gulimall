6b48511fc3dfd41400bb2ab54d0f14bfec87f1fb
gmy1234 <1508594767@qq.com>
2022-03-22 15:22:26 +0800

    OSS 获取服务的签名

--- gulimall-product/src/test/java/com/gmy/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/gmy/gulimall/product/GulimallProductApplicationTests.java
@@ -5,10 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
