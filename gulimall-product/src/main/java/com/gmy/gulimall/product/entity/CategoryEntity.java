34ceb014668bdb3b524bd9cf840294ca9557de43
gmy1234 <1508594767@qq.com>
2022-03-23 16:29:27 +0800

    属性分组新增和查询

--- gulimall-product/src/main/java/com/gmy/gulimall/product/entity/CategoryEntity.java
+++ gulimall-product/src/main/java/com/gmy/gulimall/product/entity/CategoryEntity.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 /**
@@ -63,6 +64,8 @@ public class CategoryEntity implements Serializable {
 	private Integer productCount;
 
 	@TableField(exist = false)
+	/* 不为空才返回 */
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)
 	private List<CategoryEntity> children;
 
 }
--- gulimall-product/src/main/java/com/gmy/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/gmy/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gmy.gulimall.product.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,6 +13,7 @@ import com.gmy.common.utils.Query;
 import com.gmy.gulimall.product.dao.AttrGroupDao;
 import com.gmy.gulimall.product.entity.AttrGroupEntity;
 import com.gmy.gulimall.product.service.AttrGroupService;
+import org.springframework.util.StringUtils;
 
 
 @Service("attrGroupService")
@@ -24,6 +27,40 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long categoryId) {
+        if (categoryId == 0) {
+            final IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    new QueryWrapper<AttrGroupEntity>(null)
+            );
+            return new PageUtils(page);
+        }else {
+            final String key = (String) params.get("key");
+            // select * from pms_attr_group  where catelog_id = ?
+            // and (attr_group_id = key or attr_group_name = key)
+
+            final LambdaQueryWrapper<AttrGroupEntity> wrapper = new LambdaQueryWrapper<>();
+            // 三级分类Id
+            wrapper.eq(AttrGroupEntity::getCatelogId, categoryId);
+
+            if (StringUtils.hasText(key)){
+                wrapper.and( obj ->{
+                    obj.eq(AttrGroupEntity::getAttrGroupId, key)
+                            .or()
+                            .like(AttrGroupEntity::getAttrGroupName, key);
+                });
+            }
+
+            final IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    wrapper
+            );
+            return new PageUtils(page);
+        }
+
     }
 
 }
