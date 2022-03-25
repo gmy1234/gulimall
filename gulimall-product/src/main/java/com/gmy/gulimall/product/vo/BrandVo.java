4b8574a1e52d7e2026176380321e00cf279dc6de
gmy1234 <1508594767@qq.com>
2022-03-25 21:33:40 +0800

    获取分类关联的品牌

---
+++ gulimall-product/src/main/java/com/gmy/gulimall/product/vo/BrandVo.java
@@ -0,0 +1,18 @@
+package com.gmy.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @version 1.0
+ * @Description:
+ * @Author gmyDL
+ * @Date 2022/3/25 21:04
+ */
+@Data
+public class BrandVo {
+
+    private Long brandId;
+
+    private String brandName;
+
+}
