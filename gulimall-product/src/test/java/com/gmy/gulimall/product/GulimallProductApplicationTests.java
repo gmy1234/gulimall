0cb91cf87034994805083b83247b0deb901eade6
gmy <1508594767@qq.com>
2022-04-10 11:06:20 +0800

    整合redis，并测试

--- gulimall-product/src/test/java/com/gmy/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/gmy/gulimall/product/GulimallProductApplicationTests.java
@@ -3,10 +3,26 @@ package com.gmy.gulimall.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.UUID;
 
 
 @SpringBootTest
 class GulimallProductApplicationTests {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void stringRedisTest(){
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("hello", "word"+ UUID.randomUUID());
+
+        String hello = ops.get("hello");
+        System.out.println("之前保存的"  + hello);
+    }
 
     @Test
     void contextLoads() {
