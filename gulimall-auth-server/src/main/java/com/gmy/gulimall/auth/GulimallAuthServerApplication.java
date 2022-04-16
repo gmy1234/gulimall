717d34399acd026a39540ab40e77a3ce16990406
gmy <1508594767@qq.com>
2022-04-16 10:11:02 +0800

    搭建认证服务环境

---
+++ gulimall-auth-server/src/main/java/com/gmy/gulimall/auth/GulimallAuthServerApplication.java
@@ -0,0 +1,15 @@
+package com.gmy.gulimall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class GulimallAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallAuthServerApplication.class, args);
+    }
+
+}
