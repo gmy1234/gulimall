46f7d6ec15fb51955c4f8b045423dd9e1c01d5d3
gmy <1508594767@qq.com>
2022-04-21 18:33:48 +0800

    使用 Amqp 测试队列，交换机，绑定关系的创建

--- gulimall-order/src/test/java/com/gmy/guliorder/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/gmy/guliorder/order/GulimallOrderApplicationTests.java
@@ -3,13 +3,15 @@ package com.gmy.guliorder.order;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.gmy.guliorder.order.entity.OrderReturnReasonEntity;
 import com.gmy.guliorder.order.service.OrderReturnReasonService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+@Slf4j
 @SpringBootTest
 class GulimallOrderApplicationTests {
 
@@ -33,8 +35,35 @@ class GulimallOrderApplicationTests {
     }
 
     @Test
-    void rabbitMQ(){
+        /* 创建交换机 */
+    void createExchange(){
+        DirectExchange directExchange = new DirectExchange(
+                "my-exchange", true, false);
 
+        amqpAdmin.declareExchange(directExchange);
+        log.info("交换机创建OK,名字为：", directExchange.getName());
+    }
+
+    @Test
+        /* 创建队列 */
+    void createQueue(){
+        String s = amqpAdmin.declareQueue(
+                new Queue("my-queue", true, false, false));
+        System.out.println(s);
+        log.info("队列创建OK");
+    }
+
+    @Test
+    void Bind(){
+
+        // String destination,目的地  DestinationType destinationType 目的地类型,
+        // String exchange,交换机  String routingKey, 路由key
+        // @Nullable Map<String, Object> arguments
+        // 解释：将 交换机和目的地进行绑定，使用 routingKey 作为路由键
+        amqpAdmin.declareBinding(
+                new Binding("my-queue", Binding.DestinationType.QUEUE,
+                        "my-exchange", "my.java",null));
+        log.info("绑定成功");
     }
 
 }
